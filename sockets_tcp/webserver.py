e0b333ae8db1a520e1d4ac6a4902ebe7b6cde0c7
Momodou Jallow <sanujallow@gmail.com>
2022-09-18 02:19:27 -0500

    feature: webserver sends http response with file

--- sockets_tcp/webserver.py
+++ sockets_tcp/webserver.py
@@ -1,5 +1,5 @@
 # Import socket module
-from http.client import HTTPMessage, HTTPResponse
+from base64 import encode
 from socket import *
 from http import HTTPStatus
 import sys  # In order to terminate the program
@@ -8,13 +8,14 @@ serverSocket = socket(AF_INET, SOCK_STREAM)
 
 # Prepare a sever socket
 #Fill in start
-localhost = '127.0.0.1'
-assigned_port = 65015
-serverSocket.bind((localhost, assigned_port))
+LOCALHOST = '127.0.0.1'
+ASSIGEND_PORT = 65015
+serverSocket.bind((LOCALHOST, ASSIGEND_PORT))
 serverSocket.listen()
 #Fill in end
 
-
+FORMAT = 'utf_8'
+response_header = ''
 while True:
     print('The server is ready to receive')
 
@@ -22,34 +23,33 @@ while True:
 
     try:
 
-        message = connectionSocket.recv(1024).decode()
+        message = connectionSocket.recv(1024).decode(FORMAT)
 
         filename = message.split()[1]
 
         f = open(filename[1:])
+        outputdata = f.readlines()
 
-        outputdata = f.read()  # Fill in start             #Fill in end
         # send one http header line in to the socket
-        #Fill in start
-        serverSocket.recv("201".encode())
-        #Fill in end
+        response_header = "HTTP/1.1 200 OK\r\n"
+        connectionSocket.send(response_header.encode())
 
-        # Send the content of the requested file to the connection socket
+        # the content of the requested file to the connection socket
         for i in range(0, len(outputdata)):
-            connectionSocket.send(outputdata[i].encode())
-        connectionSocket.send("\r\n".encode())
-
+            connectionSocket.send(outputdata[i].encode(FORMAT))
+            connectionSocket.send("\r\n".encode(FORMAT))
         connectionSocket.close()
+        f.close()
     except IOError:
         # Send HTTP response code and message for file not found
-        #Fill in start
-
-        connectionSocket.send(
-            f"{HTTPStatus.NOT_FOUND:<10} {filename} not found".encode())
-        #Fill in end
+        response_header = "HTTP/1.1 404 Not Found \r\n\r\n "
+        response = f'<h2 style="text-align: center">Oops! 404 <br>file "{filename[1:]}" could not be found </h2>'
+        connectionSocket.send(response_header.encode())
+        connectionSocket.send(response.encode())
 
         # Close the client connection socket
         connectionSocket.close()
+        break
 
 serverSocket.close()
 sys.exit()  # Terminate the program after sending the corresponding data
